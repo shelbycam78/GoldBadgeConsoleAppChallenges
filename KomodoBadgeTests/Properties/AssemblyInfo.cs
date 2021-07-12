0ece0bbb08eec260d6b86420d3a2466086e12e43
Shelby Simpson <shelbycam78@gmail.com>
2021-07-12 18:36:23 -0400

    challenge 3 poco done

---
+++ KomodoBadgeTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KomodoBadgeTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KomodoBadgeTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0a9de29d-74b6-468f-8278-e5d3e2374a5b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ KomodoBadges.POCO/Badge.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoBadges.POCO
+{
+    public class Badge
+    {
+        public int BadgeID { get; set; }
+        public string BadgeName { get; set; }
+
+        public List<string> DoorNames = new List<string>();
+        
+
+
+        public Badge() { }
+        public Badge(int badgeID, string badgeName, List<string> doorNames)
+        { {
+                BadgeID = badgeID;
+                BadgeName = badgeName;
+                DoorNames = doorNames;
+          }
+
+
+    }
+}
